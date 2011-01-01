ebcccd045e5ca43035851e4f1d3652298b88e296
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-01-01 20:45:42 +1000

    - added Funq and added yet more mode - for running not just with TPL but also with dedicated Threads, as many as there are logical cores on the machine to limit context switching

---
+++ Funq.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Funq;
+
+namespace PaulBenchmark
+{
+	public class Funq : IPaulTest
+	{
+		private readonly Container container;
+
+		public Funq()
+		{
+			container = new Container();
+			container.Register(c => new Game()).ReusedWithin(ReuseScope.Container);
+			container.Register(c => new Player(c.Resolve<Game>(), c.Resolve<Gun>())).ReusedWithin(ReuseScope.None);
+			container.Register(c => new Gun(c.Resolve<Game>(), c.LazyResolve<Bullet>())).ReusedWithin(ReuseScope.None);
+			container.Register(c => new Bullet(c.Resolve<Game>())).ReusedWithin(ReuseScope.None);
+		}
+
+		#region IPaulTest Members
+
+		public Player ResolvePlayer()
+		{
+			return container.Resolve<Player>();
+		}
+
+		#endregion
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PaulBenchmark
@@ -12,7 +13,7 @@ namespace PaulBenchmark
 		{
 			// calling with 'c 100' will execute 100 iterations. adding 'm' will execute on many threads (using TPL Tasks)
 			var iterations = GetIterationsCount(args);
-			var multithreaded = GetIsMultithreaded(args);
+			var multithreaded = ReadMode(args);
 			var tests = GetTestSubjects();
 			// warmup
 			foreach (var paulTest in tests)
@@ -23,21 +24,55 @@ namespace PaulBenchmark
 			//so that we don't reuse singletons from the warmup run
 			tests = GetTestSubjects();
 			Console.WriteLine("Running {0} times...", iterations);
-			if (multithreaded)
+			switch (multithreaded)
 			{
-				Console.WriteLine("...on multiple threads");
-				foreach (var paulTest in tests)
-				{
-					RunMultithreaded(paulTest, iterations);
-				}
+				case Mode.Thread_based:
+					Console.WriteLine("...on multiple threads (using TPL Tasks)");
+					foreach (var paulTest in tests)
+					{
+						RunTaskBased(paulTest, iterations);
+					}
+					break;
+				case Mode.Task_based:
+					Console.WriteLine("...on multiple threads (using {0} Threads)", Environment.ProcessorCount);
+					foreach (var paulTest in tests)
+					{
+						RunThreadBased(paulTest, iterations);
+					}
+					break;
+				default:
+					foreach (var paulTest in tests)
+					{
+						Run(paulTest, iterations, true);
+					}
+					break;
 			}
-			else
+		}
+
+		private static void RunThreadBased(IPaulTest test, int count)
+		{
+			var stopwatch = Stopwatch.StartNew();
+			var threads = new Thread[Environment.ProcessorCount];
+			var interval = count/threads.Length;
+			for (int i = 0; i < threads.Length; i++)
 			{
-				foreach (var paulTest in tests)
+				var thread = new Thread(() =>
 				{
-					Run(paulTest, iterations, true);
-				}
+					for (int j = 0; j < interval; j++)
+					{
+						var player = test.ResolvePlayer();
+						player.Shoot();
+					}
+				});
+				thread.Name = test.GetType().Name + " " + (i + 1);
+				threads[i] = thread;
+				thread.Start();
 			}
+			for (var i = 0; i < threads.Length; i++)
+			{
+				threads[i].Join();
+			}
+			Console.WriteLine(" Hey {0} - you did it in {1}", test.GetType().Name, stopwatch.Elapsed);
 		}
 
 		private static IPaulTest[] GetTestSubjects()
@@ -50,11 +85,12 @@ namespace PaulBenchmark
 			       		new Autofac(),
 			       		new Autofac_delegates(),
 			       		new Unity(),
-			       		new StructureMap()
+			       		new StructureMap(),
+			       		new Funq()
 			       	};
 		}
 
-		private static void RunMultithreaded(IPaulTest test, int count)
+		private static void RunTaskBased(IPaulTest test, int count)
 		{
 			var stopwatch = Stopwatch.StartNew();
 			var tasks = new Task[count];
@@ -70,10 +106,19 @@ namespace PaulBenchmark
 			Console.WriteLine(" Hey {0} - you did it in {1}", test.GetType().Name, stopwatch.Elapsed);
 		}
 
-		private static bool GetIsMultithreaded(string[] args)
+		private static Mode ReadMode(string[] args)
 		{
 			var isMultithreaded = Array.IndexOf(args, "m") != -1;
-			return isMultithreaded;
+			if (isMultithreaded)
+			{
+				return Mode.Task_based;
+			}
+			var isThreadBased = Array.IndexOf(args, "t") != -1;
+			if (isThreadBased)
+			{
+				return Mode.Thread_based;
+			}
+			return Mode.Single_threaded;
 		}
 
 		private static void Run(IPaulTest test, int count, bool measure)
@@ -104,5 +149,12 @@ namespace PaulBenchmark
 			}
 			return int.Parse(args[indexOfCount + 1]);
 		}
+	}
+
+	public enum Mode
+	{
+		Single_threaded,
+		Task_based,
+		Thread_based
 	}
 }
